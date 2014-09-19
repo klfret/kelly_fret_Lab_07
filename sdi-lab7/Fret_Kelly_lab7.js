81265a155b665c7b1fe623f2f49725bcbac6ee06
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-18 21:48:26 -0400

    Commit 3 - Lab 7 - JS Objects Project.

--- sdi-lab7/Fret_Kelly_lab7.js
+++ sdi-lab7/Fret_Kelly_lab7.js
@@ -1,47 +1,55 @@
-alert("Lab 7 JSON Objects - JavaScript works!- 18 September 2014");
+// alert("Lab 7 JSON Objects - JavaScript works!- 18 September 2014");
 
+//Next, create two methods for the object. The first should be an acceleration 
+//method so that when the method is called, the current speed property will update 
+//by whatever amount you send into the method.  Note that this will require your 
+//method to have an argument that is sent in when the method is called.  Also keep 
+//in mind that the current speed can never exceed the top speed of the vehicle.
 
 // global variables
-var accSpeed;
-var decSpeed;
-var currentSpeed;
+var increase = 55;
+var decrease;
+var resultSpeed;
 
 // object
-var myDrmCar = {
+var myDreamCar = {
 		
-			make:       "Nissan",
-			model:      "Altima",
-			color:      "Maroon",
-			curSpeed:   0,
-			topSpeed:   150,
-			getAccSpeed: function(accel) {
-				var incSpeed;
+			make:           "Nissan",
+			model:          "Altima",
+			color:          "Maroon",
+			currentSpeed:   0,
+			topSpeed:       175,
+			getAccSpeed:    function(acceleration) {
 				
-				this.incSpeed = this.curSpeed + this.accel;	
+				this.currentSpeed = acceleration;
+				
+				if (this.currentSpeed <= this.topSpeed) {
+					
+					//this.currentSpeed = this.currentSpeed + acceleration;
+					console.log("The current speed is " + this.currentSpeed + "mph.");
+					
+				} else {
+					
+					console.log("Not allow to exceed the vehicle's top speed limit of " + this.topSpeed + "mph.");
+				}					
 			
-				return accel;
+			return this.currentSpeed;
 				
-				}	
+			}
 };
 
 
 // main code
-console.log("My dream car is a " + myDrmCar.color + ", " + myDrmCar.make + ", " + myDrmCar.model + ".");
+console.log("My dream car is a " + myDreamCar.color + ", " + myDreamCar.make + " " + myDreamCar.model + ".");
+console.log("This vehicle has a top speed of " + myDreamCar.topSpeed + "mph.");
 
-var speedArray = [70, 100,151];
 
-for (var i = 0; i < speedArray.length; i++) {
+for (var i = 0; i < 3; i++) {
 	
- 		currentSpeed = myDrmCar.getAccSpeed(speedArray[i]);
-		console.log("The current speed is " + currentSpeed + " mph.");
+	returnedSpeed = myDreamCar.getAccSpeed(increase);
+	increase = increase + returnedSpeed;
+	console.log(increase);
 };
-
-
-
-
-
-
-
 
 
 
