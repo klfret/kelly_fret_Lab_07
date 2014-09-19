463613ce73ec7349b234b3796c12b8748d907c69
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-19 19:49:50 -0400

    Commit 12 - Lab 7 - JS Objects Project.

--- sdi-lab7/Fret_Kelly_lab7.js
+++ sdi-lab7/Fret_Kelly_lab7.js
@@ -1,11 +1,16 @@
-// alert("Lab 7 JSON Objects - JavaScript works!- 18 September 2014");
+// alert("Lab 7 Objects - JavaScript works!- 18 September 2014");
+
+
+// Kelly L. Fret
+// SDI 1409
+// Lab 7 - Objects
+// 19 September 2014
 
 
 
 // global variables
-var increase = 20;
-var decrease = 20;
-//var returnedAccSpeed;
+var increase = 30;
+var decrease = 35;
 var returnedDecSpeed;
 
 
@@ -18,32 +23,35 @@ var myDreamCar = {
 			currentSpeed:   0,
 			topSpeed:       175,
 			
+			// acceleration method
 			getAccSpeed: function(acceleration) {
 								
 				this.currentSpeed = acceleration;
 				
 				if (this.currentSpeed <= this.topSpeed) {
 					
+					// if currentSpeed is less than or equal to topSpeed, output message
 					console.log("The current accelerated speed is " + this.currentSpeed + " mph.");
 					
 				} else {
 					
-					// if user's speed acceleration is greater than the top speed, output message and assign topSpeed to currentSpeed
+					// if speed acceleration is greater than the top speed, output message and assign topSpeed to currentSpeed
 					console.log("Invalid speed. The speed cannot exceed the vehicle's top speed of " + this.topSpeed + " mph.");
 					
 					this.currentSpeed = this.topSpeed;
 				}					
 				
+			// when method's process is complete, return currentSpeed's value
 			return this.currentSpeed;
 				
 		},
 
-	
+		// deceleration method
 		getDecSpeed: function(deceleration) {
 		
 			this.currentSpeed = this.currentSpeed - deceleration;
 			
-			// if user's speed deceleration is less than 0, output message and assign 0 to currentSpeed
+			// if speed deceleration is less than 0, output message and assign 0 to currentSpeed
 			if (this.currentSpeed < 0) {
 				
 				console.log("Invalid speed. The speed cannot be less than 0.");
@@ -51,6 +59,7 @@ var myDreamCar = {
 				this.currentSpeed = 0;
 			}
 			
+		// when method's process is complete, return currentSpeed's value
 		return this.currentSpeed;
 		
 		}
@@ -58,7 +67,9 @@ var myDreamCar = {
 
 
 // main code
+// output initial values
 console.log("My dream car is a " + myDreamCar.color + " or White, fully loaded, " + myDreamCar.make + " " + myDreamCar.model + ".");
+console.log("\n");
 console.log("The vehicle is parked with a current speed of " + myDreamCar.currentSpeed + " mph, but has a top speed of " + myDreamCar.topSpeed + " mph.");
 console.log("\n");
 
@@ -66,31 +77,31 @@ console.log("\n");
 // for loop to call the getAccSpeed method and increase the speed
 for (var i = 0; i < 3; i++) {
 	
-	// increase = parseInt(prompt("Enter number to accelerate speed: ", "0"));
+	// accelerate increase's values by 30
+	increase += 30;
 	
+	// call getAccSpeed method
 	myDreamCar.getAccSpeed(increase);
-	
-	increase+=20;
 	
 };
 
 console.log("\n");
+// output current speed
 console.log("The current speed is now " + myDreamCar.currentSpeed + " mph.");
+
 
 // for loop to call the getDecSpeed method and decrease the speed
 for (var i = 0; i < 3; i++) {
 	
-	// decrease = parseInt(prompt("Enter number to decelerate speed: ", "5"));
-
+	// decelerate decrease's values by 5
+	decrease -= 5;
+	
+	// call getDecSpeed method
 	returnedDecSpeed = myDreamCar.getDecSpeed(decrease);
 	
-	console.log(returnedDecSpeed);  //  *************
-	decrease-=5;
-	
-	
-	
 	if (returnedDecSpeed <= 0) {
-	
+		
+		// bring vehicle to complete stop
 		console.log("The vehicle has come to a complete stop.")	;
 		break;
 	
@@ -99,4 +110,5 @@ for (var i = 0; i < 3; i++) {
 };
 
 console.log("\n");
+// output final current speed
 console.log("After deceleration the final current speed is " + returnedDecSpeed + " mph.");
